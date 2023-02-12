ddc2084921c3a4736748cf4d7a4bfddb9199fc54
whee1337 <tkaiser92@gmx.de>
2023-02-12 16:50:54 +0100

    adds grey theme

--- app/utils/themeTTC.ts
+++ app/utils/themeTTC.ts
@@ -3,6 +3,7 @@ import { MantineThemeOverride } from "@mantine/core";
 const themeTTC: MantineThemeOverride = {
    colors: {
         brand: ['#FFE5E5', '#FFB8B8', '#FF8A8A', '#FF5C5C', '#FF2E2E', '#FF0000', '#CC0000', '#990000', '#660000','#330000' ],
+        grey: ['#FFE5E5', '#FFB8B8', '#FF8A8A', '#FF5C5C', '#FF2E2E', '#FF0000', '#CC0000', '#990000', '#660000','#330000' ],
     },
   colorScheme: "light",
   primaryColor: 'brand',
